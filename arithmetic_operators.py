0d64cb123458d4e8b52eb27ba91f6ca3fb570ca3
lmallya <97362183+lmallya@users.noreply.github.com>
2022-07-09 22:08:47 -0700

    Add files via upload

---
+++ arithmetic_operators.py
@@ -0,0 +1,22 @@
+# addition(+), subtraction(-), multiplication(*), division(/), floor division(//), modulo(%), exponent(**)
+
+# addition(+)
+3 + 2, 9 + 10, 10.5 + 4.5 # 5, 19, 15.0
+
+# subtraction(-)
+3 - 2, 9 - 10, 10.5 - 4.5 # 1, -1, 6.0
+
+# multiplication(*)
+3 * 2, 9 * 10, 10.5 * 4.5 # 6, 90, 47.25
+
+# division(/) --- quotient
+3 / 2, 9 / 10, 10.5 / 4.5 # 1.5, 0.9, 2.3
+
+#floor division(//) --- int(quotient)
+3 // 2, 9 // 10, 10.5 // 4.5 # 1, 0, 2
+
+#modulo(%) --- remainder
+3 % 2, 9 % 10, 10.5 % 4.5 # 1, 9, 1.5
+
+#exponent(**)
+3 ** 2, 9 ** 10, 10.5 ** 4.5 # 9, 3486784401, 39386.904117720595
